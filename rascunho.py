035b7238ec6802955a11d78f6455765753e041c5
Puro Osso <ppaschoal42@gmail.com>
2023-10-31 09:47:27 -0300

    31/10

--- rascunho.py
+++ rascunho.py
@@ -1,3 +1,3 @@
 #teste led
-sudo apt install python3-rpi.gpio
-https://peppe8o.com/use-a-8x8-led-matrix-with-raspberry-pi-and-python/
+#sudo apt install python3-rpi.gpio
+## https://peppe8o.com/use-a-8x8-led-matrix-with-raspberry-pi-and-python/
